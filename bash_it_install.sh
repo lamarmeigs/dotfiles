c53a443f43fa9b3aec36dddb29a4d559fcf6e131
lamarmeigs <lmeigs@shiftgig.com>
2016-06-14 12:04:37 -0500

    Create custom bash-it install script
    Bash-It's install script modifies .bash_profile directly, which defeats the
    purpose of this project. The custom script merely enables my prefered aliases,
    plugins, and auto-completions.

---
+++ bash_it_install.sh
@@ -0,0 +1,15 @@
+# Enable select aliases
+ln -sf "$BASH_IT/aliases/available/fuck.aliases.bash" "$BASH_IT/aliases/enabled/fuck_aliases.bash"
+ln -sf "$BASH_IT/aliases/available/general.aliases.bash" "$BASH_IT/aliases/enabled/general.aliases.bash"
+ln -sf "$BASH_IT/aliases/available/vim.aliases.bash" "$BASH_IT/aliases/enabled/vim.aliases.bash"
+
+# Enable completion aliases
+ln -sf "$BASH_IT/completion/available/bash-it.completion.bash" "$BASH_IT/completion/enabled/bash-it.completion.bash"
+ln -sf "$BASH_IT/completion/available/brew.completion.bash" "$BASH_IT/completion/enabled/brew.completion.bash"
+ln -sf "$BASH_IT/completion/available/django.completion.bash" "$BASH_IT/completion/enabled/django.completion.bash"
+ln -sf "$BASH_IT/completion/available/git.completion.bash" "$BASH_IT/completion/enabled/git.completion.bash"
+ln -sf "$BASH_IT/completion/available/pip.completion.bash" "$BASH_IT/completion/enabled/pip.completion.bash"
+ln -sf "$BASH_IT/completion/available/vagrant.completion.bash" "$BASH_IT/completion/enabled/vagrant.completion.bash"
+
+# Enable completion aliases
+ln -sf "$BASH_IT/plugins/available/alias-completion.plugin.bash" "$BASH_IT/plugins/enabled/alias-completion.plugin.bash"
